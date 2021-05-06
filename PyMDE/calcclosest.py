61e93cf286c3fdc3d9921b72bbea9488a70db727
Gustav Moesmand <gustavmoesmand@gmail.com>
2021-05-06 21:07:12 +0200

    pre implementation of map between graph and skeleton nodes

--- PyMDE/calcclosest.py
+++ PyMDE/calcclosest.py
@@ -2,19 +2,11 @@ import numpy as np
 import pymde
 import helper_functions_hpc as hf
 from scipy.spatial import KDTree
-import _multiprocessing
+import multiprocessing
 
-if __name__ == "__main__":
-    mnist = pymde.datasets.MNIST()
-    unembedded = np.array(mnist.data)[:1000]
-    dist_matrix = hf.create_dist_matrix(unembedded, show_progress=False)
-    
-    k = 3
-    max_dist = 1700
-    neighbours = np.zeros((len(unembedded), k), dtype=int)
 
-    # Finding closest points
-    for i in range(len(unembedded)):
+def find_closest(start_idx: int, end_idx: int, neighbours, dist_matrix, k, max_dist, Q: multiprocessing.Queue):
+    for i in range(start_idx, end_idx):
         cur_row = dist_matrix[i]
         closest_nodes = np.argpartition(cur_row, k+1)[:k+1]
         closest_nodes = np.delete(closest_nodes, np.where(closest_nodes==i))
@@ -22,7 +14,39 @@ if __name__ == "__main__":
         for node in closest_nodes:
             dist = cur_row[node]
             if dist <= max_dist:
-                neighbours[i, idx] = node
+                Q.put((i, idx, node))
                 idx += 1
+
+
+if __name__ == "__main__":
+    lim = 1000
+    mnist = pymde.datasets.MNIST()
+    unembedded = np.array(mnist.data)[:lim]
+    dist_matrix = hf.create_dist_matrix(unembedded, show_progress=False)
     
-    np.savetxt("closestneighbours.csv", neighbours, delimiter=",")
+    k = 3
+    max_dist = 1700
+    neighbours = np.zeros((len(unembedded), k), dtype=int)
+    num = 4
+
+    Q = multiprocessing.Queue()
+
+    threads = []
+    for i in range(num):
+        size = lim//num
+        start = i * size
+        end = min(i * size + size, 1000)
+        p = multiprocessing.Process(target=find_closest, args=(start, end, neighbours, dist_matrix, k, max_dist, Q))
+        p.start()
+        print("Started thread: ", p.name)
+        threads.append(p)
+    
+    for p in threads:
+        p.join()
+        print("Joined thread: ", p.name)
+    # Finding closest points
+    
+    o_neighbours = np.loadtxt("closestneighbours.csv", delimiter=",")
+    print(Q.get())
+
+    # np.savetxt("closestneighbours.csv", neighbours, delimiter=",")
--- PyMDE/graphprediction.py
+++ PyMDE/graphprediction.py
@@ -1,5 +1,6 @@
+from seaborn.palettes import color_palette
 import torch
-from pygel3d import hmesh, graph, gl_display as gd
+from pygel3d import graph, gl_display as gd
 import numpy as np
 import pymde
 from tqdm import tqdm
@@ -12,23 +13,35 @@ import seaborn as sns
 import pandas as pd
 
 class GraphAnalysis:
-    
+    _results_path = r"E:/GIT/Bachelor/PyMDE/Results/"
+    _dist_matrix_path = r"E:/GIT/Bachelor/PyMDE/DistMatrix/"
+
+
     # Loads mnist numbers.
     # Only loads certain labels, if labels specified (Default=ALL)
     # Only loads a certain amount if limit set (Default=1000)
-    def __init__(self,  limit=1000, 
-                        labels=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 
-                        preserve_neighbours=True, 
-                        dist_matrix=None
+    def __init__(self,  limit:int = 1000, 
+                        labels:np.array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 
+                        preserve_neighbours:bool = True, 
+                        dist_matrix:np.array = None,
+                        forget_labels:float = None,
                         ):
 
         print("Loading dataset")
         mnist = pymde.datasets.MNIST()
         self._mnist = mnist
-        self.limit = limit
-        mnist_labels = mnist.attributes["digits"][:limit]
-        self._labels = mnist_labels[np.in1d(mnist_labels, labels)]
+        self._limit = limit
         
+        mnist_labels = mnist.attributes["digits"][:limit]
+        if forget_labels is not None:
+            np.random.seed(1)
+            n = int(mnist_labels.shape[0]*forget_labels)
+            random_indices = np.random.choice(mnist_labels.shape[0], n, replace=False)
+            mnist_labels[random_indices] = -1
+
+        self._labels = mnist_labels[np.in1d(mnist_labels, labels)]
+
+        # Important otherwise distances will be uint
         unembedded = np.array(mnist.data)[:limit].astype("float")
         self._unembedded = unembedded[np.in1d(mnist_labels, labels)]
 
@@ -50,13 +63,16 @@ class GraphAnalysis:
         for n, ppos in zip(self._g.nodes(), self._embedded):
             pos[n] = ppos
         
-        print("Creating dist-matrix")
+
         # Creating dist_matrix
         if dist_matrix is not None:
             self._dist_matrix = dist_matrix
+            print("Loaded dist matrix")
         else:
+            print("Creating dist matrix")
             arr = self._unembedded
             self._dist_matrix = hf.create_dist_matrix(arr)
+            self.save_distmatrix()
         print("Symmetric dist_matrix", np.allclose(self._dist_matrix.transpose(), self._dist_matrix))
         self.print_seperator_line()
 
@@ -93,21 +109,23 @@ class GraphAnalysis:
     def connect_graph(self, neighbours=3, max_dist=1700): 
         hf.connect_k_closest_points(self._g, self._dist_matrix, k=neighbours, max_dist=max_dist)
     def skeletonize_graph(self): 
-        self._skeleton = graph.LS_skeleton(self._g)
-        tree = KDTree(self._embedded)
+        self._skeleton, self._skel_map = graph.LS_skeleton_and_map(self._g)
+    
+        
+
+    # Current method 
+    def relabel_skeleton(self):
+        # Empty array for storing the new labels for each index of the skeleton
+        self._skel_labels = np.zeros(len(self._skeleton.nodes()))
+        self._skel_non_embed = np.zeros((len(self._skeleton.nodes()), 784))
         self._skeleton.cleanup()
 
-        skel_labels = np.zeros(len(self._skeleton.nodes()))
-        skel_non_embed = np.zeros((len(self._skeleton.nodes()), 784))
-
+        tree = KDTree(self._embedded)
         pos = self._skeleton.positions()
         for n in self._skeleton.nodes():
-            _, idx = tree.query(pos[n])
-            skel_labels[n] = self._labels[idx]
-            skel_non_embed[n] = self._unembedded[idx]
-
-        self._skel_labels = skel_labels
-        self._skel_unembed_pos = skel_non_embed
+            dist, idx = tree.query(pos[n])
+            self._skel_labels[n] = self._labels[idx]
+            self._skel_non_embed[n] = self._unembedded[idx]
 
     # MISC
     def print_seperator_line(self):
@@ -115,22 +133,55 @@ class GraphAnalysis:
         print("".join("=" for _ in range(40)))
     
     # Accurary testing
-    def test_accuracy(self, test_size=1000):
-        Y_test = self._mnist.attributes["digits"][self.limit:self.limit + test_size]
-        x_test = self._mnist.data[self.limit:self.limit + test_size].numpy()
+    def test_accuracy(self, test_size=1000, show=True):
+        Y_test = self._mnist.attributes["digits"][self._limit:self._limit + test_size]
+        x_test = self._mnist.data[self._limit:self._limit + test_size].numpy()
         guess = list()
         print("Testing accurazy of skeleton")
         for idx, val in tqdm(enumerate(x_test)):
-            dist, nearest_idx = hf.nearest_neighbours(self._skel_unembed_pos, val)
+            dist, nearest_idx = hf.nearest_neighbours(self._skel_non_embed, val)
             guess.append((self._skel_labels[nearest_idx], Y_test[idx]))
-        
+
+                
         tru, fal = 0, 0
         for gue in guess:
             if gue[0] == gue[1]: tru+=1
             else: fal+= 1
+        self._test_accuracy = (tru)/(tru+fal)
+
+        conf_matrix = np.zeros((10, 10))
+        for gue in guess:
+            predic = int(gue[0])
+            truth = int(gue[1])
+            conf_matrix[truth, predic] += 1
         
-        accuracy = (tru)/(tru+fal)
-        print(accuracy)
+        self._conf_matrix = conf_matrix    
+        if show: 
+            plt.show()
+
+    def visualize_tests(self, show=True):
+        if not hasattr(self, "_conf_matrix"):
+            raise Exception("Tests not conducted yet.")        
+
+        test_size = np.sum(self._conf_matrix)
+        df_cm = pd.DataFrame(self._conf_matrix, range(10), range(10))
+        sns.set(font_scale=1.2)
+        plt.suptitle(f"Accurazy: {self._test_accuracy}")
+        plt.xlabel("Prediction")
+        plt.ylabel("Truth")
+
+        color_map = sns.color_palette("PuBu")
+        sns.heatmap(df_cm,
+                    annot=True, 
+                    cmap=color_map, 
+                    annot_kws={"size": 14}, 
+                    fmt="g").set_title(f"Training size: {self._limit}, Testing size {test_size}")
+        
+        os.listdir(self._results_path)
+        
+        name = "PredictHeatmap" + "_Num" + str(self._limit) + "_Testpoints"
+        plt.savefig(self._results_path + "PredictHeatmap.png", format="png")
+
 
     # Fields
     def labels(self):       return self._labels
@@ -146,18 +197,20 @@ class GraphAnalysis:
         graph.save("graph.graph", self._g)
     def save_skeleton(self):
         graph.save("skeleton.graph", self._skeleton)
-
+        
     def save_distmatrix(self):
-        np.savetxt("distmatrix.csv", self._dist_matrix, delimiter=",")
+        np.savetxt(f"{self._dist_matrix_path}distmatrix_{self._limit}.csv", self._dist_matrix, delimiter=",")
     @staticmethod
-    def load_distmatrix():
-        return np.loadtxt("distmatrix.csv", delimiter=",")
+    def load_distmatrix(path):
+        return np.loadtxt(path, delimiter=",")
 
 
 if __name__ == "__main__":
-    ganal = GraphAnalysis(limit=2000)
+    ganal = GraphAnalysis(limit=1000)
     
+
     ganal.connect_graph(max_dist=1700, neighbours=1)
     ganal.skeletonize_graph()
-    ganal.test_accuracy()
-    
+    ganal.relabel_skeleton()
+    ganal.test_accuracy(test_size=1000, show=False)
+    ganal.visualize_tests(show=False)
